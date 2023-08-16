7459da232b9751f1c73825c9265759462b358e13
Tony Grimes <acidtone@tonygrimes.com>
2023-08-16 12:12:27 -0600

    closes #1

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,27 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	
+	preprocess: vitePreprocess(
+		mdsvex({
+			// The default mdsvex extension is .svx; this overrides that.
+			extensions: ['.md'],
+
+			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
+		})
+	),
+
 	vitePlugin: {
-		inspector: true,   
+		inspector: true,
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
