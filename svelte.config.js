79b97fa46df540c22dd9b160febee8a861cdd3ec
lilyx13 <ashlyn@lilyx.io>
2023-08-16 22:31:42 -0600

    formatting
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,33 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
+import adapter from "@sveltejs/adapter-auto";
+import {vitePreprocess} from "@sveltejs/kit/vite";
+import {mdsvex} from "mdsvex";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
+  extensions: [".svelte", ".md"],
 
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(
-		mdsvex({
-			// The default mdsvex extension is .svx; this overrides that.
-			extensions: ['.md'],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(
+    mdsvex({
+      // The default mdsvex extension is .svx; this overrides that.
+      extensions: [".md"],
 
-			// Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
-			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
-		})
-	),
+      // Adds IDs to headings, and anchor links to those IDs. Note: must stay in this order to work.
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+    })
+  ),
 
-	vitePlugin: {
-		inspector: true,
-	},
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  vitePlugin: {
+    inspector: true,
+  },
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+  },
 };
 export default config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,16 @@
-import { join } from 'path'
-import typography from '@tailwindcss/typography'
-import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
+import {join} from "path";
+import typography from "@tailwindcss/typography";
+import skeleton from "@skeletonlabs/skeleton/tailwind/skeleton.cjs";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [typography,...skeleton()],
-}
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}"),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, ...skeleton()],
+};
