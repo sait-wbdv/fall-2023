67d17d8a83244947e7c8924faed3f3987d4bab90
lilyx13 <ashlyn@lilyx.io>
2023-08-18 16:41:17 -0600

    TOC and layout to nav
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/routes/courses/[code]/day-[day]/+page.svelte
+++ src/routes/courses/[code]/day-[day]/+page.svelte
@@ -1,34 +1,43 @@
 <script>
-  import { page }  from '$app/stores';
-  import { lessonsById } from "$lib/stores";
-  import dayjs from 'dayjs';
+  import {TableOfContents} from "@skeletonlabs/skeleton";
+  import {page} from "$app/stores";
+  import {lessonsById} from "$lib/stores";
+  import dayjs from "dayjs";
   export let data;
 
   const lesson = $lessonsById[`${$page.params.code}/${$page.params.day}`];
 </script>
+
 <nav class="page-nav">
-  <ul>
+  <ul class="flex justify-around">
     {#if lesson.prev}
-    <li><a href="/courses/{lesson.prev}">Prev</a></li>
+      <li><a href="/courses/{lesson.prev}">Prev</a></li>
     {:else}
-    <li><span>Prev</span></li>
+      <li><span>Prev</span></li>
     {/if}
     <li>
-      <time datetime={lesson.date}>{dayjs(lesson.date).format('dddd, MMM D')}</time>
+      <time datetime={lesson.date}>{dayjs(lesson.date).format("dddd, MMM D")}</time>
     </li>
     {#if lesson.next}
-    <li><a href="/courses/{lesson.next}">Next</a></li>
+      <li><a href="/courses/{lesson.next}">Next</a></li>
     {:else}
-    <li><span>Next</span></li>
-    {/if}  
+      <li><span>Next</span></li>
+    {/if}
   </ul>
 </nav>
 
 <p>{lesson.codeLabel} - Day {lesson.day}</p>
-{#if lesson.status === 'draft'}
-<h1>To be Announced</h1>
-<p>This lesson has not been released.</p>
+{#if lesson.status === "draft"}
+  <h1>To be Announced</h1>
+  <p>This lesson has not been released.</p>
 {:else}
-<h1>{lesson.title}</h1>
-{@html data.lessonContent}
+  <header>
+    <h1>{lesson.title}</h1>
+  </header>
+  <div class="flex">
+    <article>
+      {@html data.lessonContent}
+    </article>
+    <TableOfContents allowedHeadings="h2" />
+  </div>
 {/if}
