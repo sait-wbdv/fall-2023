2ec9ec2122e3f7240d213292672547ce629ad1bb
lilyx13 <ashlyn@lilyx.io>
2023-08-18 17:21:03 -0600

    content styling
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/routes/courses/[code]/day-[day]/+page.svelte
+++ src/routes/courses/[code]/day-[day]/+page.svelte
@@ -11,17 +11,17 @@
 <nav class="page-nav my-6 container mx-auto">
   <ul class="flex justify-between">
     {#if lesson.prev}
-      <li><a href="/courses/{lesson.prev}">Prev</a></li>
+      <li><a href="/courses/{lesson.prev}" class="btn variant-filled rounded-md">Prev</a></li>
     {:else}
-      <li><span>Prev</span></li>
+      <li><a href="/courses/{lesson.prev}" class="btn variant-outline rounded-md">Prev</a></li>
     {/if}
     <li>
       <time datetime={lesson.date}><strong>{dayjs(lesson.date).format("dddd, MMM D")}</strong></time>
     </li>
     {#if lesson.next}
-      <li><a href="/courses/{lesson.next}">Next</a></li>
+      <li><a href="/courses/{lesson.prev}" class="btn variant-filled rounded-md">Next</a></li>
     {:else}
-      <li><span>Next</span></li>
+      <li><span class="btn varient-outline rounded-md">Next</span></li>
     {/if}
   </ul>
 </nav>
@@ -31,13 +31,14 @@
   <h1>To be Announced</h1>
   <p>This lesson has not been released.</p>
 {:else}
-  <header>
-    <h1>{lesson.title}</h1>
+  <header class="my-4 lg:my-6">
+    <h1 class="h1 mb-2">{lesson.title}</h1>
+    <p>{lesson.excerpt}</p>
   </header>
   <div class="flex justify-between">
     <article class="mx-auto container">
       {@html data.lessonContent}
     </article>
-    <TableOfContents allowedHeadings="h2" class="hidden lg:block" />
+    <TableOfContents allowedHeadings="h2" rounded="rounded-md" active="bg-primary-800/20" class="hidden lg:block" />
   </div>
 {/if}
