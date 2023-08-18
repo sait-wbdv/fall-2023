bf951334d8fa9d4b897b7afeff941a6f6425c758
lilyx13 <ashlyn@lilyx.io>
2023-08-18 17:30:45 -0600

    style prop for footer vs header layout
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,7 +7,7 @@
   <section
     class="flex flex-col lg:flex-row gap-4 justify-between lg:w-4/6 mt-3 lg:mt-6 container lg:mx-auto mx-4 mb-4 lg:mb-0"
   >
-    <MainNav />
+    <MainNav styles="flex-col" />
     <nav>
       <ul>
         <li>
