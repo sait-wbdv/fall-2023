3d1c06ec08117fa6bd3e1bbb5c67fa21cf1b5846
lilyx13 <ashlyn@lilyx.io>
2023-08-18 17:20:32 -0600

    hr and footer content aligned
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
 
 <footer>
   <section
-    class="flex flex-col lg:flex-row gap-4 justify-between lg:justify-around mt-3 lg:mt-6 container lg:mx-auto mx-4 mb-4 lg:mb-0"
+    class="flex flex-col lg:flex-row gap-4 justify-between lg:w-4/6 mt-3 lg:mt-6 container lg:mx-auto mx-4 mb-4 lg:mb-0"
   >
     <MainNav />
     <nav>
