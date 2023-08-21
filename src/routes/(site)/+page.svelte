6059bee23b1607bccc47f1ebfc2c794071dccff3
lilyx13 <ashlyn@lilyx.io>
2023-08-21 15:57:11 -0600

    spacing in main page header
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -6,7 +6,9 @@
 <svelte:head>
   <title>{siteTitle}</title>
 </svelte:head>
-<section class="schedule">
-  <h1>Program Schedule</h1>
+<section>
+  <header class="ml-3 mt-4 lg:mt-6 mb-3">
+    <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-error-300 to-primary-400">Program Schedule</h1>
+  </header>
   <Schedule />
 </section>
