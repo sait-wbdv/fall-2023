6e4c0453286f644f1511efa2741b8e8ff235be9c
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2023-08-21 17:27:32 -0600

    Visual hierarchy improvements (#21)
    * global font sizes w/typography plugin
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * footer xl size
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * schedule color + space hierarchy
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * spacing in main page header
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * typofix
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    ---------
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/lib/components/Schedule.svelte
+++ src/lib/components/Schedule.svelte
@@ -4,33 +4,43 @@
   import dayjs from "dayjs";
 </script>
 
+<!-- TODO: use autocollapse + open props dyanmically for the current week -->
 {#each $schedule as week, index}
-  <section class="week">
-    <Accordion>
+  <section class="lg:my-2">
+    <Accordion rounded="rounded-md" spacing="space-y-3">
       <AccordionItem>
         <svelte:fragment slot="summary">
-          <h2 id="week-{index + 1}">Week {index + 1}</h2>
+          <h2 class="h3" id="week-{index + 1}">Week {index + 1}</h2>
         </svelte:fragment>
         <svelte:fragment slot="content">
           {#each week as day}
             {#if day.type === "lesson" && day.status != "draft"}
-              <time>{dayjs(day.date).format("dddd, MMM D")}</time>
-              <a href="courses/{day.code}/day-{day.day}">
+              <div class="bg-gradient-to-r from-error-300 to-primary-400 bg-clip-text text-transparent mt-1">
+                <time class="font-bold">{dayjs(day.date).format("dddd, MMM D")}</time>
+              </div>
+              <a
+                href="courses/{day.code}/day-{day.day}"
+                class="hover:bg-primary-800/20 rounded-md block transition duration-150 ease-linear py-2 pl-2"
+              >
                 <article class="{day.code} lesson">
                   <p>{day.code.toUpperCase().replace("-", " ")} - Day {day.day}</p>
-                  <h3>{day.title}</h3>
+                  <h3 class="text-primary-300">{day.title}</h3>
                 </article>
               </a>
             {:else if day.status === "draft"}
-              <time>{dayjs(day.date).format("dddd, MMM D")}</time>
-              <article class="{day.code} unpublished">
+              <div class="bg-gradient-to-r from-error-300 to-primary-400 bg-clip-text text-transparent mt-1">
+                <time class="font-bold">{dayjs(day.date).format("dddd, MMM D")}</time>
+              </div>
+              <article class="{day.code} unpublished pl-2">
                 <p>{day.code.toUpperCase().replace("-", " ")} - Day {day.day}</p>
-                <h3 class={day.code}>To be Announced</h3>
+                <h3 class="text-slate-500">To be Announced</h3>
               </article>
             {:else}
-              <time>{dayjs(day.date).format("dddd, MMM D")}</time>
-              <article class="holiday">
-                <h3 class="holiday">{day.label}</h3>
+              <div class="bg-gradient-to-r from-error-300 to-primary-400 bg-clip-text text-transparent mt-1">
+                <time class="font-bold">{dayjs(day.date).format("dddd, MMM D")}</time>
+              </div>
+              <article class="holiday pl-2">
+                <h3 class="text-slate-500">{day.label}</h3>
               </article>
             {/if}
           {/each}
