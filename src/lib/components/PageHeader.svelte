5d8aed4b735959143859e94f1cee9552c1dc401a
lilyx13 <ashlyn@lilyx.io>
2023-08-28 18:32:44 -0600

    simple page header component w prop & slot input
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

---
+++ src/lib/components/PageHeader.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let title = "Default H1 Title";
+  export let description = "A brief descriptive paragraph.";
+</script>
+
+<header class="mt-4 xl:mt-12">
+  <h1 class="mb-2 xl:mb-3 text-transparent bg-gradient-to-r from-pink-500/80 to-secondary-600 bg-clip-text">
+    {title}
+  </h1>
+  <p class="text-lg xl:text-xl">{description}</p>
+  <slot />
+</header>
