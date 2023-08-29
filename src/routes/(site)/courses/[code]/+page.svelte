763e62c420afaf2adebc606599329927147b2451
lilyx13 <ashlyn@lilyx.io>
2023-08-28 18:33:09 -0600

    applied page header component test 1
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/routes/(site)/courses/[code]/+page.svelte
+++ src/routes/(site)/courses/[code]/+page.svelte
@@ -1,92 +1,87 @@
 <script>
-  import { page }  from '$app/stores';
-  import { courses } from '$lib/stores';
-  import dayjs from 'dayjs';
-
-	export let data;
+  import {page} from "$app/stores";
+  import {courses} from "$lib/stores";
+  import dayjs from "dayjs";
+  import PageHeader from "$lib/components/PageHeader.svelte";
+  export let data;
 
   const course = $courses.find((item) => item.code === $page.params.code);
 </script>
-<h1>{course.codeLabel} - {course.title}</h1>
-<p>{course.excerpt}</p>
+
+<PageHeader title={`${course.codeLabel} - ${course.title}`} description={course.excerpt} />
+
 <h2>Lesson Schedule</h2>
 <ul>
   {#each course.days as date, index}
-    <li><a href="{course.code}/day-{index + 1}">Day {index + 1} - {dayjs(date).format('MMMM D')}</a></li>
+    <li><a href="{course.code}/day-{index + 1}">Day {index + 1} - {dayjs(date).format("MMMM D")}</a></li>
   {/each}
 </ul>
 
 <h2>Assessments</h2>
-{#if data.code === 'cpnt-262'}
-<!-- <h3>Course Breakdown</h3>
+{#if data.code === "cpnt-262"}
+  <!-- <h3>Course Breakdown</h3>
 <ul>
   <li><strong>Part 1 - Vanilla Javascript: 40%</strong></li>
   <li><strong>Part 2 - VueJS and Nuxt: 60%</strong></li>
 </ul> -->
-
 {/if}
 
 {#if data.assessments.length}
-	{#each data.assessments as assessment}
-		{#if assessment.type === 'assignment' || assessment.type === 'achievements'}
-			{#if assessment.status === 'published'}
-				<a class="assessment" href="{course.code}/assessments/{assessment.slug}">
-					<article class="{course.code}">
-						<h3>{assessment.title}</h3>
-						<p>Weight: {assessment.points}%</p>
-						{#if assessment.due}
-							<p>Due: {assessment.due}</p>
-						{:else}
-							<p>Due: TBA</p>
-						{/if}
-					</article>
-				</a>
-			{:else}
-				<article>
-					<h3>{assessment.title}</h3>
-					<p>Weight: {assessment.points}%</p>
-					{#if assessment.due}
-						<p>Due: {assessment.due}</p>
-					{:else}
-						<p>Due: TBA</p>
-					{/if}
-				</article>
-			{/if}
-		{/if}
-	{/each}
+  {#each data.assessments as assessment}
+    {#if assessment.type === "assignment" || assessment.type === "achievements"}
+      {#if assessment.status === "published"}
+        <a class="assessment" href="{course.code}/assessments/{assessment.slug}">
+          <article class={course.code}>
+            <h3>{assessment.title}</h3>
+            <p>Weight: {assessment.points}%</p>
+            {#if assessment.due}
+              <p>Due: {assessment.due}</p>
+            {:else}
+              <p>Due: TBA</p>
+            {/if}
+          </article>
+        </a>
+      {:else}
+        <article>
+          <h3>{assessment.title}</h3>
+          <p>Weight: {assessment.points}%</p>
+          {#if assessment.due}
+            <p>Due: {assessment.due}</p>
+          {:else}
+            <p>Due: TBA</p>
+          {/if}
+        </article>
+      {/if}
+    {/if}
+  {/each}
 {:else}
-<p>To Be Announced</p>
+  <p>To Be Announced</p>
 {/if}
 
 <style>
   a.assessment {
     text-decoration: none;
   }
-  
+
   article {
     margin: 1rem 2rem;
     background: #eee;
     padding: 1rem;
-    border-radius: .5rem;
+    border-radius: 0.5rem;
   }
 
   a article {
     color: hsl(var(--hue), 20%, 10%);
     background: hsl(var(--hue), 25%, 93%);
 
-    box-shadow: 0px 1px 1px rgba(0,0,0,0.05),
-    0px 2px 2px rgba(0,0,0,0.05),
-    0px 4px 4px rgba(0,0,0,0.05),
-    0px 8px 8px rgba(0,0,0,0.05);
+    box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.05), 0px 2px 2px rgba(0, 0, 0, 0.05), 0px 4px 4px rgba(0, 0, 0, 0.05),
+      0px 8px 8px rgba(0, 0, 0, 0.05);
   }
 
   a:hover article {
-    box-shadow: 0px 1px 1px rgba(0,0,0,0.1),
-    0px 2px 2px rgba(0,0,0,0.1),
-    0px 4px 4px rgba(0,0,0,0.1),
-    0px 8px 8px rgba(0,0,0,0.1);
+    box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1), 0px 2px 2px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1),
+      0px 8px 8px rgba(0, 0, 0, 0.1);
   }
-
 
   article.dsgn-270 {
     --hue: 270;
