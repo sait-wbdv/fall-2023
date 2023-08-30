671c7ec158a0032be1aed77d3df740b4eb300554
lilyx13 <ashlyn@lilyx.io>
2023-08-30 15:35:31 -0600

    cleaner and more useful footer design applied
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,24 +1,31 @@
 <script>
   import MainNav from "./MainNav.svelte";
   import {siteAuthor} from "$lib/config";
+  import Icon from "@iconify/svelte";
 </script>
 
-<footer class="mx-4">
-  <section class="flex justify-between max-w-4xl mx-auto xl:px-2">
-    <MainNav styles="flex-col" />
-    <nav>
-      <ul>
-        <li>
-          <a href="/api/rss.xml" rel="external">RSS</a>
-        </li>
-        <li>
-          <a href="/">Home</a>
-        </li>
-      </ul>
-    </nav>
+<footer class="mx-4 mt-6">
+  <section class="flex flex-col items-center">
+    <img src="images/sait-logo.png" alt="Sait Logo" class="w-16 mb-2" />
+    <MainNav styles="gap-2" />
   </section>
-  <hr class="opacity-80 max-w-6xl mx-auto xl:w-full xl:px-2" />
-  <section class="text-center my-3 xl:my-6">
-    <p>&copy;{new Date().getFullYear()} {siteAuthor}</p>
+  <hr class="opacity-80 max-w-2xl mx-auto xl:w-full xl:px-2 mb-4" />
+  <section>
+    <div class="text-center my-2">
+      <p>&copy;{new Date().getFullYear()} {siteAuthor}</p>
+    </div>
+    <ul class="flex justify-center gap-2 lg:gap-4 mb-4">
+      <li>
+        <a href="https://codepen.io/acidtone" target="_blank"><Icon icon="fa6-brands:codepen" class="text-xl" /></a>
+      </li>
+      <li>
+        <a href="https://github.com/sait-wbdv" target="_blank"><Icon icon="fa6-brands:github" class="text-xl" /></a>
+      </li>
+      <li>
+        <a href="https://www.figma.com/team_invite/redeem/iied98q0sW8p9gSDqm1hML" target="_blank"
+          ><Icon icon="fa6-brands:figma" class="text-xl" /></a
+        >
+      </li>
+    </ul>
   </section>
 </footer>
