ad2b7811785748fcadc50948a4645d06793ee35b
lilyx13 <ashlyn@lilyx.io>
2023-09-01 17:20:29 -0600

    navItem hover and active theme like footer
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -1,25 +1,21 @@
 <script>
-	import { currentPage, isMenuOpen } from '$lib/assets/js/store'
+  import {currentPage, isMenuOpen} from "$lib/assets/js/store";
 
-	export let href
+  export let href;
 
-	$: isCurrentPage = $currentPage.startsWith(href)
+  $: isCurrentPage = $currentPage.startsWith(href);
 
-	const maybeCloseMenu = () => {
-		if (href != $currentPage) {
-			isMenuOpen.set(false)
-		}
-	}
+  const maybeCloseMenu = () => {
+    if (href != $currentPage) {
+      isMenuOpen.set(false);
+    }
+  };
 </script>
 
-
-<li>
-	<a
-		href={href}
-		on:click={maybeCloseMenu}
-		class:active={isCurrentPage}
-		aria-current={isCurrentPage ? 'page' : false}
-	>
-		<slot />
-	</a>
+<li
+  class="dark:hover:text-primary-500 dark:active:text-primary-300 hover:text-primary-700 active:text-primary-500 transition duration-150 ease-linear"
+>
+  <a {href} on:click={maybeCloseMenu} class:active={isCurrentPage} aria-current={isCurrentPage ? "page" : false}>
+    <slot />
+  </a>
 </li>
