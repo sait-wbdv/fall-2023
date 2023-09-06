d8afea947a94497bf7b4e256baa4454c1adc4141
lilyx13 <ashlyn@lilyx.io>
2023-09-05 21:58:32 -0600

    assignment and day 2 notes
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

---
+++ src/lib/content/assessments/dsgn-270/due-dates.js
@@ -0,0 +1,27 @@
+export const dsgn270DueDates = [
+  {
+    slug: 'assignment-1',
+    due: 'Monday September 11 @ 11:59PM',
+    status: 'published'
+  },
+  {
+    slug: 'assignment-2',
+    due: ' Sunday January 29 @ 11:59PM',
+    status: 'published'
+  },
+  {
+    slug: 'assignment-3',
+    due: 'Friday February 17 @ 11:59PM',
+    status: 'published'
+  },
+  {
+    slug: 'assignment-4',
+    due: 'Tuesday February 21 @ 11:59PM',
+    status: 'published'
+  },
+  {
+    slug: 'achievements',
+    due: 'Daily @ 5:00PM',
+    status: 'published'
+  }
+];
