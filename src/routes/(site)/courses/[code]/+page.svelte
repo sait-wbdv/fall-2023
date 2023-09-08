53b26f2c7fa79e7c35b652b57ac34d8c225c71ad
lilyx13 <ashlyn@lilyx.io>
2023-09-08 12:29:04 -0600

    visual hierarchy to course page overviews
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/routes/(site)/courses/[code]/+page.svelte
+++ src/routes/(site)/courses/[code]/+page.svelte
@@ -9,32 +9,48 @@
 </script>
 
 <PageHeader title={`${course.codeLabel} - ${course.title}`} description={course.excerpt} />
-
-<h2>Lesson Schedule</h2>
-<ul>
-  {#each course.days as date, index}
-    <li>
-      <a href="{course.code}/day-{index + 1}">Day {index + 1} - {dayjs(date).format("MMMM D")}</a>
-      <p>{course.excerpt}</p>
-    </li>
-  {/each}
-</ul>
-
-<h2>Assessments</h2>
-{#if data.code === "cpnt-262"}
-  <!-- <h3>Course Breakdown</h3>
+<section class="container pl-4">
+  <h2 class="h2">Lesson Schedule</h2>
+  <ul>
+    {#each course.days as date, index}
+      <li>
+        <a
+          href="{course.code}/day-{index + 1}"
+          class="h4 dark:hover:text-primary-500 dark:active:text-primary-300 hover:text-primary-700 active:text-primary-500 transition duration-150 ease-linear"
+          >Day {index + 1} - {dayjs(date).format("MMMM D")}
+        </a>
+        <p class="max-w-prose">{course.excerpt}</p>
+      </li>
+    {/each}
+  </ul>
+</section>
+<section class="container pl-4">
+  <h2>Assessments</h2>
+  {#if data.code === "cpnt-262"}
+    <!-- <h3>Course Breakdown</h3>
 <ul>
   <li><strong>Part 1 - Vanilla Javascript: 40%</strong></li>
   <li><strong>Part 2 - VueJS and Nuxt: 60%</strong></li>
 </ul> -->
-{/if}
+  {/if}
 
-{#if data.assessments.length}
-  {#each data.assessments as assessment}
-    {#if assessment.type === "assignment" || assessment.type === "achievements"}
-      {#if assessment.status === "published"}
-        <a class="assessment" href="{course.code}/assessments/{assessment.slug}">
-          <article class={course.code}>
+  {#if data.assessments.length}
+    {#each data.assessments as assessment}
+      {#if assessment.type === "assignment" || assessment.type === "achievements"}
+        {#if assessment.status === "published"}
+          <a class="assessment" href="{course.code}/assessments/{assessment.slug}">
+            <article class={course.code}>
+              <h3>{assessment.title}</h3>
+              <p>Weight: {assessment.points}%</p>
+              {#if assessment.due}
+                <p>Due: {assessment.due}</p>
+              {:else}
+                <p>Due: TBA</p>
+              {/if}
+            </article>
+          </a>
+        {:else}
+          <article>
             <h3>{assessment.title}</h3>
             <p>Weight: {assessment.points}%</p>
             {#if assessment.due}
@@ -43,20 +59,10 @@
               <p>Due: TBA</p>
             {/if}
           </article>
-        </a>
-      {:else}
-        <article>
-          <h3>{assessment.title}</h3>
-          <p>Weight: {assessment.points}%</p>
-          {#if assessment.due}
-            <p>Due: {assessment.due}</p>
-          {:else}
-            <p>Due: TBA</p>
-          {/if}
-        </article>
+        {/if}
       {/if}
-    {/if}
-  {/each}
-{:else}
-  <p>To Be Announced</p>
-{/if}
+    {/each}
+  {:else}
+    <p>To Be Announced</p>
+  {/if}
+</section>
