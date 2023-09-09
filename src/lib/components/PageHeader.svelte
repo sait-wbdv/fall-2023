28e7269c7287b255307ae76654c21dc3616c701f
lilyx13 <ashlyn@lilyx.io>
2023-09-08 18:19:23 -0600

    style changes to pageheader
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -3,12 +3,12 @@
   export let description = "A brief descriptive paragraph.";
 </script>
 
-<header class="mt-4 xl:mt-12 mb-4 xl:mb-8 mx-4 xl:mx-0">
+<header class="mt-4 xl:mt-12 mb-4 xl:mb-8 mx-4 xl:mx-0 pl-4">
   <h1
     class="mb-2 xl:mb-3 text-transparent bg-gradient-to-r from-primary-500 to-pink-500 dark:from-primary-400/80 dark:to-tertiary-600 bg-clip-text"
   >
     {title}
   </h1>
-  <p class="text-lg xl:text-xl">{description}</p>
+  <p class="text-lg xl:text-xl max-w-prose">{description}</p>
   <slot />
 </header>
