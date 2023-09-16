8a466289a9b05052c87822ea1133bae4fd0ed3a6
Ashlyn <77300739+lilyx13@users.noreply.github.com>
2023-09-15 19:53:29 -0600

    Dsgn 270 days 3 and 4 (#43)
    * exceprt for day 1
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * syllabus type orientation content
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dsgn-d1 outlining
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * init assessment files
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * assignment names and excerpts
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * better default visual hierarchy text
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dsgn-270 graded activity instructions
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * day 1 dsgn-270
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * spacing in lesson plan left side
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * day 2 homework
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * day 2 hw
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * day 1 communication info
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * images for slides
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * hide default due
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * assignment and day 2 notes
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * day 2 final activity
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dsgn due dates
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * assessment outlines
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * added excerpt to course overview
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dsgn content
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * visual hierarchy to course page overviews
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * hover and sidebar consistent hierarchy for interaction
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * removed excerpt as it was the wrong one
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * style changes to pageheader
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dsgn prep and assignment info
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * style changes to day page
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * dsgn-3 slide images
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * day 3 lecture notes
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * more images
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    * day 3 and 4 design
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>
    
    ---------
    
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- src/lib/due-dates.js
+++ src/lib/due-dates.js
@@ -1,6 +1,6 @@
 import {cpnt201DueDates} from "$lib/content/assessments/cpnt-201/due-dates.js";
 import {dsgn270DueDates} from "$lib/content/assessments/dsgn-270/due-dates.js";
-import { cpnt260DueDates } from '$lib/content/assessments/cpnt-260/due-dates.js'
+import {cpnt260DueDates} from "$lib/content/assessments/cpnt-260/due-dates.js";
 // import { cpnt262DueDates } from '$lib/content/assessments/cpnt-262/due-dates.js'
 // import { cpnt200DueDates } from '$lib/content/assessments/cpnt-200/due-dates.js'
 // import { cpnt265DueDates } from '$lib/content/assessments/cpnt-265/due-dates.js'
@@ -15,8 +15,8 @@ export const dueDates = [
     dueDates: dsgn270DueDates,
   },
   {
-    code: 'cpnt-260',
-    dueDates: cpnt260DueDates
+    code: "cpnt-260",
+    dueDates: cpnt260DueDates,
   },
   // {
   //   code: 'cpnt-262',
